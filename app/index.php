941d4d2921af5360774f0af68a658f466ec3c391
totyka97 <tamas970906@gmail.com>
2024-04-07 19:37:50 +0200

    Főoldal v2

--- app/index.php
+++ app/index.php
@@ -2,10 +2,13 @@
 <html lang="hu">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Tatár Tamás Portfólió</title>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+
     <link rel="stylesheet" type="text/css" href="/style/style.css">
     <script src="Scripts/typewrite.js" type="text/javascript"></script>
 </head>
--- app/view/header.php
+++ app/view/header.php
@@ -6,7 +6,7 @@
   <div class="collapse navbar-collapse " id="navbarNav">
     <ul class="navbar-nav ">
       <li class="nav-item active">
-        <a class="nav-link text-white" href="/about">Rólam <span class="sr-only">(current)</span></a>
+        <a class="nav-link text-white" href="/about">Rólam</a>
       </li>
       <li class="nav-item">
         <a class="nav-link text-white" href="/contact">Elérhetőségek</a>
